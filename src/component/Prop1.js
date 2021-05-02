eed0c6e2c86e76be866553625949ad421f20eb19
undisputed679 <mayankbhushan679@gmail.com>
2021-05-02 23:20:46 +0530

    class-usestate

--- src/component/Prop1.js
+++ src/component/Prop1.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Prop2 from './Prop2'
 
-function Prop1() {
-    return (
-        <div>
-            <Prop2 name="max" age="28" />
-            <Prop2 name="milan" age="47" />
-            <Prop2 name="roger" age="37" />
-        </div>
-    )
-}
+export default class Prop1 extends Component {
+    state = {
+        persons: [
+            {name:"max",age:"28"},
+            {name:"milan",age:"34"},
+            {name:"roger",age:"23"}
+        ]
+    }
 
-export default Prop1
+    switchnameHandler = () =>{
+        console.log("i am clicked");
+        this.setState({
+            persons: [
+                {name:"name changed",age:"28"},
+                {name:"milan",age:"34"},
+                {name:"roger",age:"age changed"}
+            ]
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h1>this is also react page</h1>
+                <button onClick={this.switchnameHandler}>switch name</button>
+                <Prop2 name={this.state.persons[0].name} age={this.state.persons[0].age} />
+                <Prop2 name={this.state.persons[1].name} age={this.state.persons[1].age} />
+                <Prop2 name={this.state.persons[2].name} age={this.state.persons[2].age} />
+            </div>
+        )
+    }
+}
--- src/component/Prop2.js
+++ src/component/Prop2.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Prop2(props) {
     return (
         <div>
-            <h1>hii i am{props.name} and i am {props.age} years old this is the use of props </h1>
+            <h1>hii i am {props.name} and i am {props.age} years old this is the use of props </h1>
         </div>
     )
 }
